cf2fcad00a7bc513b4b981d7feff514a40701153
Nabiilah Putri Safa <putri@Nabiilahs-MacBook-Pro.local>
2026-01-07 17:20:59 +0700

    chore: initial nuxt project

---
+++ nuxt.config.ts
@@ -0,0 +1,11 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt', 'lucide-nuxt'],
+  runtimeConfig: {
+    public: {
+      apiBase: 'http://localhost:8000/api/' 
+    }
+  },
+})
