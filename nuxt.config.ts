3f92341c022a8ee803ee981b9242d9926f0f07ab
Nabiilah Putri Safa <putri@Nabiilahs-MacBook-Pro.local>
2026-01-07 22:05:21 +0700

    ci: setup github actions with linting, typechecking, and sonarcloud

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt', 'lucide-nuxt'],
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@pinia/nuxt',
+    'lucide-nuxt',
+    '@nuxt/eslint',
+  ],
   runtimeConfig: {
     public: {
       apiBase: 'http://localhost:8000/api/' 
